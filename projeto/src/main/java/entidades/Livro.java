d589895b85dba96d03787f479acfae569b2e7868
Pietro <162758840+PietroAlexandria@users.noreply.github.com>
2025-09-09 23:49:13 +0000

    Metade feito

---
+++ projeto/src/main/java/entidades/Livro.java
@@ -0,0 +1,10 @@
+package entidades;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Livro {
+
+}
