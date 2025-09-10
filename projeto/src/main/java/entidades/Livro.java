9cffca6b817d49f4d946d74af4af0276c2e123c1
Pietro <162758840+PietroAlexandria@users.noreply.github.com>
2025-09-10 01:00:29 +0000

    Finalização da criação de classes .java

--- projeto/src/main/java/entidades/Livro.java
+++ projeto/src/main/java/entidades/Livro.java
@@ -3,8 +3,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String titulo;
+
+    public String autor;
+
+    public Integer anoPublicacao;
+
+    public String isbn;
+
+    @ManyToOne
+    public Editora editora;
 
 }
